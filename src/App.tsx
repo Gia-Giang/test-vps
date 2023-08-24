ce91919e32e0a25d2372ed395fe7db697e726614
Gia-Giang <95181249+Gia-Giang@users.noreply.github.com>
2023-08-25 00:17:55 +0700

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React native
+          Learn React native ne
         </a>
       </header>
     </div>
